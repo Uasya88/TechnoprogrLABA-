034233659548ef71ff6b036d6f68b80f6d9263a4
Nikita <Waldeer@yandex.ru>
2015-09-18 13:02:38 +0400

    3.1

--- main.cpp
+++ main.cpp
@@ -2,12 +2,14 @@
 //Это учебная программа
 int main()
 {
-    int n,m,sum,razn;
+    int n,m,sum,razn,proizv;
     cout<< "VVedite dva chisla\n";
     cin>>n;
     cin>>m;
     sum=n+m;
     razn=n-m;
+    proizv=n*m;
     cout<<"summa"<<" "<<sum<<"\n ";
-    cout<<"raznost"<<" "<<razn;
+    cout<<"raznost"<<" "<<razn<<"\n";
+    cout<<"proizv"<<" "<<proizv;
 }
